b1489cb4a612671a1baba15e7df38949d6fb4c1f
dee <aggrey256@gmail.com>
2018-09-06 19:31:30 +0300

    added javascript for the edit-modal

--- js/main.js
+++ js/main.js
@@ -1,17 +1,29 @@
 var modal = document.getElementById('add-modal');
+var updatemodal=document.getElementById('editmodal')
 
 
 var btn = document.getElementById("add-btn");
+var editbtn=document.getElementById('editmodal-btn')
 
 
+// display modal
 btn.onclick = function() {
     modal.style.display = "block";
+};
+
+editbtn.onclick=function(){
+updatemodal.style.display='block';
 }
 
-
-
+// Close Modal
 window.onclick = function(event) {
     if (event.target == modal) {
         modal.style.display = "none";
+    }
+}
+
+window.onclick = function(event) {
+    if (event.target == updatemodal) {
+        updatemodal.style.display = "none";
     }
 }
