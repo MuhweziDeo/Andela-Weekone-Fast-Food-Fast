06555e9b87c2f935f3ffa5aa2d6646a9f0af02bd
dee <aggrey256@gmail.com>
2018-09-07 19:51:38 +0300

    fixed broken js

--- js/main.js
+++ js/main.js
@@ -5,27 +5,27 @@ var updatemodal=document.getElementById('editmodal')
 var btn = document.getElementById("add-btn");
 var editbtn=document.getElementById('editmodal-btn')
 var closebtn=document.getElementById('close')
-
+var closebtn2=document.getElementById('close2')
 
 // display modal
 btn.onclick = function() {
     modal.style.display = "block";
+
 };
+editbtn.onclick=function(){
+    updatemodal.style.display='block';
+    }
+    
 // close modal
-closebtn.onclick= function(){
+closebtn2.onclick= function(){
     updatemodal.style.display='none';
 }
 closebtn.onclick= function(){
     modal.style.display='none';
 }
 
-editbtn.onclick=function(){
-updatemodal.style.display='block';
-}
 
 // Close Modal
-
-
 window.onclick = function(event) {
     if (event.target == updatemodal) {
         updatemodal.style.display = "none";
