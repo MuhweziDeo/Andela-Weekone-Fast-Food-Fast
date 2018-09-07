69e95ddbefcb160c3612d43b710d0d1b45690523
dee <aggrey256@gmail.com>
2018-09-07 20:07:38 +0300

    added javasctipt functions to display modals

--- js/main.js
+++ js/main.js
@@ -1,33 +1,46 @@
 var modal = document.getElementById('add-modal');
-var updatemodal=document.getElementById('editmodal')
+var updatemodal = document.getElementById('editmodal')
 
 
 var btn = document.getElementById("add-btn");
-var editbtn=document.getElementById('editmodal-btn')
-var closebtn=document.getElementById('close')
-var closebtn2=document.getElementById('close2')
+var editbtn = document.getElementById('editmodal-btn')
+var editbtn2 = document.getElementById('editmodal-btn2')
+var editbtn3 = document.getElementById('editmodal-btn3')
+var editbtn4 = document.getElementById('editmodal-btn4')
+var editbtn5 = document.getElementById('editmodal-btn5')
+var closebtn = document.getElementById('close')
+var closebtn2 = document.getElementById('close2')
 
 // display modal
-btn.onclick = function() {
+btn.onclick = function () {
     modal.style.display = "block";
 
 };
-editbtn.onclick=function(){
-    updatemodal.style.display='block';
-    }
-    
-// close modal
-closebtn2.onclick= function(){
-    updatemodal.style.display='none';
+
+editbtn.onclick = function () {
+    updatemodal.style.display = 'block';
 }
-closebtn.onclick= function(){
-    modal.style.display='none';
+editbtn2.onclick = function () {
+    updatemodal.style.display = 'block';
+}
+editbtn3.onclick = function () {
+    updatemodal.style.display = 'block';
+}
+editbtn4.onclick = function () {
+    updatemodal.style.display = 'block';
+}
+editbtn5.onclick = function () {
+    updatemodal.style.display = 'block';
+}
+
+
+// close modal
+closebtn2.onclick = function () {
+    updatemodal.style.display = 'none';
+}
+closebtn.onclick = function () {
+    modal.style.display = 'none';
 }
 
 
 // Close Modal
-window.onclick = function(event) {
-    if (event.target == updatemodal) {
-        updatemodal.style.display = "none";
-    }
-}
