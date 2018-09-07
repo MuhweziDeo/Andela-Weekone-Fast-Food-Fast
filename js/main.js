0578ab607334152618aaa528f4854f882a6b2f0d
dee <aggrey256@gmail.com>
2018-09-07 19:39:43 +0300

    added script to close modal

--- js/main.js
+++ js/main.js
@@ -4,23 +4,27 @@ var updatemodal=document.getElementById('editmodal')
 
 var btn = document.getElementById("add-btn");
 var editbtn=document.getElementById('editmodal-btn')
+var closebtn=document.getElementById('close')
 
 
 // display modal
 btn.onclick = function() {
     modal.style.display = "block";
 };
+// close modal
+closebtn.onclick= function(){
+    updatemodal.style.display='none';
+}
+closebtn.onclick= function(){
+    modal.style.display='none';
+}
 
 editbtn.onclick=function(){
 updatemodal.style.display='block';
 }
 
 // Close Modal
-window.onclick = function(event) {
-    if (event.target == modal) {
-        modal.style.display = "none";
-    }
-}
+
 
 window.onclick = function(event) {
     if (event.target == updatemodal) {
