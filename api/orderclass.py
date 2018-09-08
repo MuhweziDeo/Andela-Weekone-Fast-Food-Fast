6941c6b1643e5b0c331770057056c5a1f3a6de54
dee <aggrey256@gmail.com>
2018-09-08 21:11:31 +0300

    [#160364273] added order-class

---
+++ api/orderclass.py
@@ -0,0 +1,11 @@
+class Order(object):
+	# init class
+	def __init__(self):
+		self.idcounter=0
+		self.orders=[]
+
+	def create_order(slef,data):
+		order=data
+		order['id']=self.idcounter=self.idcounter+1
+		self.orders.append(order)
+		return order
