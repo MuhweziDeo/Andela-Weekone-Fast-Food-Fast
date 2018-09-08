367f5cae876ffaf06e7adc2d824046943050716c
dee <aggrey256@gmail.com>
2018-09-08 21:46:17 +0300

    added route for posting an order

--- api/routes.py
+++ api/routes.py
@@ -1,7 +1,23 @@
-from flask_restplus import Resource,fields
-from app import app
+from flask_restplus import Api,Resource,fields
+from .app import app
+from .orderclass import Order
 
 api=Api(app, prefix="/fastfoods/api/v1")
 
+order=Order()
 
+orderobj=api.model('Order',{
+	
+	'meal':fields.String,
+	'user':fields.String,
+	'location':fields.String,
+	'quantity':fields.Integer
+	})
 
+@api.route('/orders')
+class Orders(Resource):
+
+	@api.expect(orderobj)
+	def post(self):
+		data=api.payload
+		return order.create_order(data),201
