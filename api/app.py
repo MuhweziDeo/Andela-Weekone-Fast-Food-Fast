e2391ac9e8f8396b030d616f264b6ffbe79fc58e
dee <aggrey256@gmail.com>
2018-09-08 20:46:47 +0300

    added run file and updated prefix for api in app

--- api/app.py
+++ api/app.py
@@ -3,8 +3,6 @@ from flask_restplus import Api
 
 
 app = Flask(__name__)
-api=Api(app, prefix="fastfoods/api/v1")
+api=Api(app, prefix="/fastfoods/api/v1")
 
 
-if __name__=='__main__':
-	app.run(debug=True)
