99c2ffd13c765a23ba313b7fd8320a6e3d1b4f33
muhwezideo <aggrey256@gmail.com>
2018-09-21 11:40:01 -0400

    added javascript to show order modal

--- js/main.js
+++ js/main.js
@@ -1,8 +1,10 @@
 var modal = document.getElementById('add-modal');
 var updatemodal = document.getElementById('editmodal')
+var ordermodal=document.getElementById('order-modal')
 
 
 var btn = document.getElementById("add-btn");
+var orderbtn=document.getElementById('place-order')
 var editbtn = document.getElementById('editmodal-btn')
 var editbtn2 = document.getElementById('editmodal-btn2')
 var editbtn3 = document.getElementById('editmodal-btn3')
@@ -10,8 +12,12 @@ var editbtn4 = document.getElementById('editmodal-btn4')
 var editbtn5 = document.getElementById('editmodal-btn5')
 var closebtn = document.getElementById('close')
 var closebtn2 = document.getElementById('close2')
+var cancelorder = document.getElementById('cancel-order')
 
 // display modal
+orderbtn.onclick=function(){
+    ordermodal.style.display="block";
+}
 btn.onclick = function () {
     modal.style.display = "block";
 
@@ -35,12 +41,12 @@ editbtn5.onclick = function () {
 
 
 // close modal
+cancelorder.onclick = function (){
+    ordermodal.style.display ='none';
+}
 closebtn2.onclick = function () {
     updatemodal.style.display = 'none';
 }
 closebtn.onclick = function () {
     modal.style.display = 'none';
 }
-
-
-// Close Modal
