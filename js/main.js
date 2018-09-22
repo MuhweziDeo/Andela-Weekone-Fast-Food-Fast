92ff39ec7f8c3cf8c2a0e8f5fc8517e8ee245b13
muhwezideo <aggrey256@gmail.com>
2018-09-22 12:50:39 -0400

    added functions to display edit fastfood modal

--- js/main.js
+++ js/main.js
@@ -1,44 +1,3 @@
-var modal = document.getElementById('add-modal');
-var updatemodal = document.getElementById('editmodal')
-var ordermodal=document.getElementById('order-modal')
-
-
-var btn = document.getElementById("add-btn");
-var orderbtn=document.getElementById('place-order')
-
-var editbtn = document.getElementById('editmodal-btn')
-var editbtn2 = document.getElementById('editmodal-btn2')
-var editbtn3 = document.getElementById('editmodal-btn3')
-var editbtn4 = document.getElementById('editmodal-btn4')
-var editbtn5 = document.getElementById('editmodal-btn5')
-var closebtn = document.getElementById('close')
-var closebtn2 = document.getElementById('close2')
-
-
-// display modal
-orderbtn.onclick=function(){
-    ordermodal.style.display="block";
-}
-btn.onclick = function () {
-    modal.style.display = "block";
-
-};
-
-editbtn.onclick = function () {
-    updatemodal.style.display = 'block';
-}
-editbtn2.onclick = function () {
-    updatemodal.style.display = 'block';
-}
-editbtn3.onclick = function () {
-    updatemodal.style.display = 'block';
-}
-editbtn4.onclick = function () {
-    updatemodal.style.display = 'block';
-}
-editbtn5.onclick = function () {
-    updatemodal.style.display = 'block';
-}
 
 // close modal
 
@@ -47,6 +6,10 @@ closebtn2.onclick = function () {
 }
 closebtn.onclick = function () {
     modal.style.display = 'none';
+}
+
+function showmodal() {
+    document.getElementById("editmodal").style.display = "block";
 }
 
 function displaymenu() {
