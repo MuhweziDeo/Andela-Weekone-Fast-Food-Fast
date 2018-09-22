8ba69ac388d8ee91d3692cad7bf853d67e4fc8fe
muhwezideo <aggrey256@gmail.com>
2018-09-22 13:09:00 -0400

    added function to display order modal and made some comments

--- js/main.js
+++ js/main.js
@@ -1,19 +1,13 @@
 
-// close modal
-
-closebtn2.onclick = function () {
-    updatemodal.style.display = 'none';
-}
-closebtn.onclick = function () {
-    modal.style.display = 'none';
-}
-
+// edit-modal
 function showmodal() {
     document.getElementById("editmodal").style.display = "block";
 }
 function closemodal() {
     document.getElementById("editmodal").style.display = "none";
 }
+
+// add fastfood modal
 
 function addfastfood() {
     document.getElementById("add-modal").style.display = "block";
@@ -22,7 +16,17 @@ function close_addmodal() {
     document.getElementById("add-modal").style.display = "none";
 }
 
+// make order modal
+function show_ordermodal() {
+    document.getElementById('order-modal').style.display = "block";
+}
 
+function cancelorder() {
+    document.getElementById("order-modal").style.display = "none";
+}
+
+
+// menu modal
 
 function displaymenu() {
     document.getElementById("menu-modal").style.display = "block";
@@ -30,7 +34,4 @@ function displaymenu() {
 
 function closemenu() {
     document.getElementById("menu-modal").style.display = "none";
-}
-function cancelorder() {
-    document.getElementById("order-modal").style.display = "none";
 }
